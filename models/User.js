4389b1cae78951ff71231adef840645b8b194fd0
Garrison <79775636+segarrison@users.noreply.github.com>
2021-11-01 18:23:51 -0500

    files for all models and code for user.js

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  verifyPword(loginPass) {
+    return bcrypt.compareSync(loginPass, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserInfo) => {
+        newUserInfo.password = await bcrypt.hash(newUserInfo.password, 10);
+        return newUserInfo;
+      },
+      beforeUpdate: async (updatedUserInfo) => {
+        updatedUserInfo.password = await bcrypt.hash(updatedUserInfo.password, 10);
+        return updatedUserInfo;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;

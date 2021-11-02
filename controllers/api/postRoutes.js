612a6c9283d3c3bf07311a3beb1847b1bfe2d370
Garrison <79775636+segarrison@users.noreply.github.com>
2021-11-01 23:35:28 -0500

    updated comment and post Routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,38 +1,60 @@
-const router = require('express').Router();
-const { Post } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
+//New post
+router.post("/", withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
-      ...req.body,
+    const newPost = await Post.create({
+      post_title: req.body.title,
+      post_content: req.body.content,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+//Delete by ID
+router.delete("/:id", withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const postData = await Post.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+//Update by ID
+router.put('/:id', withAuth, async (req, res) =>{
+    try{
+        const postData = await Post.update({
+            where:{
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+    if (!postData){
+        res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+    res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 module.exports = router;

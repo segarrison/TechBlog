81c656543c072adb91462b310576881ca5c0871e
segarrison <79775636+segarrison@users.noreply.github.com>
2021-11-29 23:51:53 -0600

    adding comment route backend

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
-const { Comment } = require("../../models");
+const { Comment, Post, User } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [{ model: Post }, { model: User }],
+    });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports =router;
